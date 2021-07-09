db5bb99d96fbbad742d54157e3aa3565c5723e6a
Vera Bertalan <vbertalan@abesse.hu>
2021-07-09 15:39:41 +0200

    Creating file structure

---
+++ Server/Options/TwilioSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Twilio.Video.Server.Options
+{
+    public class TwilioSettings
+    {
+    }
+}
