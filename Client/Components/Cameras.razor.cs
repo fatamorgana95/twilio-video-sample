787f5aa932f0187d0d44cb21a38a9fff3ff815d1
Vera Bertalan <vbertalan@abesse.hu>
2021-07-09 16:28:45 +0200

    Finish setup

--- Client/Components/Cameras.razor.cs
+++ Client/Components/Cameras.razor.cs
@@ -1,11 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Blazor.Twilio.Video.Client.Interop;
+using Blazor.Twilio.Video.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System.Threading.Tasks;
 
 namespace Blazor.Twilio.Video.Client.Components
 {
     public partial class Cameras
     {
+        [Inject]
+        protected IJSRuntime? JavaScript { get; set; }
+
+        [Parameter]
+        public EventCallback<string> CameraChanged { get; set; }
+
+        protected Device[]? Devices { get; private set; }
+        protected CameraState State { get; private set; }
+        protected bool HasDevices => State == CameraState.FoundCameras;
+        protected bool IsLoading => State == CameraState.LoadingCameras;
+
+        string? _activeCamera;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Devices = await JavaScript.GetVideoDevicesAsync();
+            State = Devices != null && Devices.Length > 0
+                    ? CameraState.FoundCameras
+                    : CameraState.Error;
+        }
+
+        protected async ValueTask SelectCamera(string deviceId)
+        {
+            await JavaScript.StartVideoAsync(deviceId, "#camera");
+            _activeCamera = deviceId;
+
+            if (CameraChanged.HasDelegate)
+            {
+                await CameraChanged.InvokeAsync(_activeCamera);
+            }
+        }
     }
 }
--- Client/Interop/VideoJS.cs
+++ Client/Interop/VideoJS.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Blazor.Twilio.Video.Shared;
+using Microsoft.JSInterop;
 using System.Threading.Tasks;
 
 namespace Blazor.Twilio.Video.Client.Interop
 {
-    public class VideoJS
+    public static class VideoJS
     {
+        public static ValueTask<Device[]> GetVideoDevicesAsync(
+              this IJSRuntime? jsRuntime) =>
+              jsRuntime?.InvokeAsync<Device[]>(
+                  "videoInterop.getVideoDevices") ?? new ValueTask<Device[]>();
+
+        public static ValueTask StartVideoAsync(
+            this IJSRuntime? jSRuntime,
+            string deviceId,
+            string selector) =>
+            jSRuntime?.InvokeVoidAsync(
+                "videoInterop.startVideo",
+                deviceId, selector) ?? new ValueTask();
+
+        public static ValueTask<bool> CreateOrJoinRoomAsync(
+            this IJSRuntime? jsRuntime,
+            string roomName,
+            string token) =>
+            jsRuntime?.InvokeAsync<bool>(
+                "videoInterop.createOrJoinRoom",
+                roomName, token) ?? new ValueTask<bool>(false);
+
+        public static ValueTask LeaveRoomAsync(
+            this IJSRuntime? jsRuntime) =>
+            jsRuntime?.InvokeVoidAsync(
+                "videoInterop.leaveRoom") ?? new ValueTask();
     }
 }
